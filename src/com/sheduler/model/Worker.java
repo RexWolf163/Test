27e4fe6e57025d4f4133afa07ff36092e171c84d
RexWolf163 <rexwolf163@gmail.com>
2015-08-19 15:27:31 +0400

    Update Worker.java

--- src/com/sheduler/model/Worker.java
+++ src/com/sheduler/model/Worker.java
@@ -1,21 +1,64 @@
+package com.scheduler.model;
+
 import java.util.ArrayList;
 import java.util.List;
 
+/** Данные работников.
+ *@version 0.2
+ * */
 public class Worker {
-	public String ID = new String();
-	public String Prof = new String();
-	public List<String> Mash = new ArrayList<String>();
+	/**Идентификатор*/
+	private String id = new String();
+	/**Профессия*/
+	private String prof = new String();
+	/**Список доступных для работы моделей станков*/
+	private List<String> mash = new ArrayList<String>();
+	/**Список отобранных операций (хранится номер в списке операций)*/
+	private List<Integer> pool = new ArrayList<Integer>();
+	/**Граница занятости в минутах от начала расчета расписания*/
+	private int workTime=0;
+	/**Номер текущей операции в списке операций*/
+	private int WorkId = -1;
+	/**Флаг, показывающий статус распределения операций. 
+	 * Если для всех операций в списке pool количество претендентов равно 1, то fin=true*/
+	private boolean fin= false;
 	
-	public List<Integer> Pool = new ArrayList<Integer>();
-	public int WorkTime=0;//занятость в минутах от начала периода
+	/**Запрос данных об id */
+	public String getId(){return this.id;}
+	/**Запрос данных об prof */
+	public String getProf(){return this.prof;}
+	/**Запрос данных об mash */
+	public List<String> getMash(){return this.mash;}
+	/**Запрос данных об pool */
+	public List<Integer> getPool(){return this.pool;}
+	/**Запрос данных об workTime */
+	public int getWorkTime(){return this.workTime;}
+	/**Запрос данных об WorkId */
+	public int getWorkId(){return this.WorkId;}
+	/**Запрос данных об fin */
+	public boolean getFin(){return this.fin;}
 	
-	//int StartTime=0;
-	public List<Integer> WorkRecord = new ArrayList<Integer>(); //запись о назначеной операции. Хранится id операции
-	
-	//Операционные переменные:
-	//int Busy = 0;
-	public int WorkWeight = 0;
-	public int WorkID = -1;
-	
-	public boolean Fin= false;
+	/**Корректировка данных id */
+	public void setId(String data){this.id=data;}
+	/**Корректировка данных prof */
+	public void setProf(String data){this.prof=data;}
+	/**Добавление данных в mash */
+	public int addMash(String data){
+		this.mash.add(data);
+		return this.mash.size();}
+	/**Добавление данных в pool */
+	public int addPool(int data){
+		this.pool.add(data);
+		return this.pool.size();}
+	/**Удаление задачи из pool*/
+	public int removePool(int data){
+		this.pool.remove(data);
+		return this.pool.size();
+	}
+	/**Корректировка данных workTime */
+	public void setWorkTime(int data){this.workTime=data;}
+	/**Корректировка данных WorkId */
+	public void setWorkId(int data){this.WorkId=data;}
+	/**Корректировка данных fin */
+	public void setFin(boolean data){this.fin=data;}
 }
