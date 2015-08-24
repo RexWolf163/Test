54e90e6b5608fc0b93efc06f1c82cb1bda771ad7
RexWolf <rexwolf163@gmail.com>
2015-08-24 08:33:09 +0400

    Update Sort.java

--- src/com/rw/util/Sort.java
+++ src/com/rw/util/Sort.java
@@ -11,23 +11,7 @@ public class Sort {
 	 * @param key Массив-ключ. Формируется, например, строкой for (Integer i:ArrayListOfValue) key.add(i.getValue());
 	 * */
 	public static void arrayMultiSort(ArrayList<?> args, ArrayList<Integer> key){
-		Integer stek = null;
-		Object stekWorker = null;
-		ArrayList<Object> ar = (ArrayList<Object>) args;
-		for (int index=0;index<key.size();index++){
-			if ((stek!=null)&&(stek>key.get(index))){
-				stekWorker=ar.get(index-1);
-				ar.set(index-1,ar.get(index));
-				ar.set(index, stekWorker);
-				stek=key.get(index-1);
-				key.set(index-1,key.get(index));
-				key.set(index, stek);
-				index-=2;
-				
-				if (index<0)index=0;
-			}
-			stek=key.get(index);
-		}
+		arrayMultiSort(args, key, false);
 	}
 	/**Сортировка массива по ключу в сторону увеличения или уменьшения значений
 	 * @param args Массив, подлежащий сортировке
