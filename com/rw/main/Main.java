4232d7fe93e489f965d35a10f701f00865fdc7f8
RexWolf <rexwolf163@gmail.com>
2015-08-27 16:38:42 +0400

    Create Main.java

---
+++ com/rw/main/Main.java
@@ -0,0 +1,13 @@
+package com.rw.main;
+
+import com.sheduler.Sheduler;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sheduler sheduler = new Sheduler();
+		sheduler.MakeShedule();
+	}
+
+}
